7a908bf60647f1df682cad26138eb74f7dcb1dc2
“asifulhaque087” <asifulhaque087@gmail.com>
2023-09-15 17:52:51 +0600

    Update homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,43 @@
 "use client";
 
-export default function Home() {
+import { UploadTypes, useFileUploader } from "@/hooks/useFileUploader";
+import { UploadPdf } from "@/components/UploadPdf";
+
+const App: React.FC = () => {
+  const {
+    inputRef: pdfInput,
+    handleClick: handlePdfClick,
+    isUploading,
+    onClick,
+    upload: uploadPdf,
+  } = useFileUploader({
+    use: UploadTypes.PDF,
+    afterUploadPdf: () => {},
+  });
+
+  const hiddenInputs = (
+    <>
+      <input
+        data-testid="pdf-input"
+        ref={pdfInput}
+        type="file"
+        name="pdf"
+        id="pdf"
+        accept="application/pdf"
+        onChange={uploadPdf}
+        onClick={onClick}
+        style={{ display: "none" }}
+      />
+    </>
+  );
+
   return (
-    <div>
-      <h1>homepage</h1>
+    <div className="m-[30px]">
+      {hiddenInputs}
+      <div>this si for file uploading</div>
+      <UploadPdf loading={isUploading} uploadPdf={handlePdfClick} />
     </div>
   );
-}
+};
+
+export default App;
